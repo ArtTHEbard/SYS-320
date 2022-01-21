222c7bdb67233271e5f57c9effa6268589263f15
ArtTHEbard <54681698+ArtTHEbard@users.noreply.github.com>
2022-01-21 17:27:01 -0500

    Week 1 Classwork

---
+++ Week01/class/syslogSSHSuccess.py
@@ -0,0 +1,30 @@
+import syslogCheck
+import importlib
+
+importlib.reload(syslogCheck)
+
+
+# SSH authentication failures
+
+def ssh_success(filename, searchterms):
+    # Call Syslog Check
+    is_found = syslogCheck._syslog(filename, searchterms)
+
+    # Found list
+    found = []
+
+    # Loop through the results
+    for eachFound in is_found:
+        # Split results
+        sp_results = eachFound.split(" ")
+
+        # Append split to found
+        found.append(sp_results[5])
+
+    # Remove duplicates
+    # and convert the list to a set.
+    hosts = set(found)
+
+    for eachHost in hosts:
+
+        print(eachHost)
