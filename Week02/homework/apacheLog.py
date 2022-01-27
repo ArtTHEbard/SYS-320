30ff248bcc8a330acb5208ed39cc72fa58f2ef1b
ArtTHEbard <54681698+ArtTHEbard@users.noreply.github.com>
2022-01-27 14:00:52 -0500

    Week 1 Classwork

---
+++ Week02/homework/apacheLog.py
@@ -0,0 +1,33 @@
+# Code by Sam Johnson
+# This code utilized the syslogCheck function to sort for domains that failed to authenticate with SSH.
+# This is done by selecting a defined split target, and appending the
+# split information to a formatted list, which is turned into a set before it is returned.
+import logCheck
+import importlib
+importlib.reload(logCheck)
+
+
+# SSH authentication failures
+
+def apache_event(filename, service, term):
+    # Call log Check
+    is_found = logCheck._logs(filename, service, term)
+
+    # Found list
+    found = []
+
+    # Loop through the results
+    for eachFound in is_found:
+        # Split results
+        sp_results = eachFound.split(" ")
+
+        # Append split to found
+        found.append(sp_results[0] + " " + sp_results[2] + " " + sp_results[4] + " Bytes Sent " + sp_results[7] + " Bytes Recieved.")
+
+    # Remove duplicates
+    # and convert the list to a set.
+    getValues = set(found)
+
+    for eachValue in getValues:
+
+        print(eachValue)
---
+++ Week02/homework/logCheck.py
@@ -0,0 +1,57 @@
+# Code by Sam Johnson
+# Create an interface to search through syslog files.
+# This is done by opening the file, storing the contents in a variable,
+# and looping through the elements within that variable, sorting them based upon inputted keywords.
+# Finally, the resutls are formatted into a sorted list and returned.
+
+import re
+import sys
+import yaml
+
+# Open the YAML file
+try:
+    with open('searchterms.yaml', 'r') as yf:
+        keywords = yaml.safe_load(yf)
+except EnvironmentError as e:
+    print(e.strerror)
+
+
+def _logs(filename, service, term):
+
+    # Queury the yaml file for term or direction and
+    # retrive the strings to search on.
+
+    terms = keywords[service][term]
+
+    listofKeywords = terms.split(",")
+
+    # Open a file
+    with open(filename) as f:
+        # Read contents of file into variable
+        contents = f.readlines()
+
+    # List to store results.
+    results = []
+
+    # Loop through the list returned.
+    for line in contents:
+
+        # Loops through the keywords.
+        for eachKeyword in listofKeywords:
+
+            # If the 'line' contains the keyword then it will print.
+            # Searches and returns results using a regular expression search.
+            x = re.findall(r'' + eachKeyword + '', line)
+
+            for found in x:
+                # Append results to result list.
+                results.append(found)
+
+    # Check to see if results exists.
+    if len(results) == 0:
+        print("No Results")
+        sys.exit()
+    # Sort List.
+    results = sorted(results)
+
+    return results
