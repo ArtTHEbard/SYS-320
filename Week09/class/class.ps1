ecf63579cc7f5777737397be2ee5d5c98d0e6ff9
ArtTHEbard <54681698+ArtTHEbard@users.noreply.github.com>
2022-03-24 13:53:36 -0400

    s

--- Week09/class/class.ps1
+++ Week09/class/class.ps1
@@ -10,13 +10,22 @@
 
 # Save the Output to a CSV fiel
 #Get-Process | Select-Object ProcessName, Id, Path | Export-Csv  -Path 'D:\Sec320\SYS-320\Week09\class\processes.csv'
-$outputname = 'D:\Sec320\SYS-320\Week09\class\services.csv'
+
+# $outputname = 'D:\Sec320\SYS-320\Week09\class\services.csv'
+
 # System Services and properties
 # Get-Service | get-member
-Get-Service | Select-Object Status, Name, DisplayName, BinaryPathName | Export-Csv -Path $outputname
+# Get-Service | Select-Object Status, Name, DisplayName, BinaryPathName | Export-Csv -Path $outputname
+
+# Get a list of running services
+$outputname = 'D:\Sec320\SYS-320\Week09\class\runningservices.csv'
+Get-Service | Where-object { $_.Status -eq "Running" } | Export-Csv $outputname
 
 # Check for output file
 if ( Test-Path -Path $outputname) {
 
-    Write-Host -BackgroundColor "Red" -foregroundcolor "white" "Services File was Created"
+    Write-Host -BackgroundColor "Green" -foregroundcolor "white" "Services File was created!"
+} else {
+
+    Write-Host -BackgroundColor "Red" -ForegroundColor "white" "Services file wasnt created."
 }
