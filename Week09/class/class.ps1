52ed3a7be59e8ee7559a3fd15f98996d3baf4c49
ArtTHEbard <54681698+ArtTHEbard@users.noreply.github.com>
2022-03-24 13:46:28 -0400

    new

--- Week09/class/class.ps1
+++ Week09/class/class.ps1
@@ -6,5 +6,17 @@
 # Get list of processes, extract name, id , and path.
 # Get-Process | Select-Object ProcessName, Id, Path
 
+# Output file variable
+
 # Save the Output to a CSV fiel
-Get-Process | Select-Object ProcessName, Id, Path | Export-Csv  -Path 'C:\Users\trini\PycharmProjects\SYS-320\Week09\class'
+#Get-Process | Select-Object ProcessName, Id, Path | Export-Csv  -Path 'D:\Sec320\SYS-320\Week09\class\processes.csv'
+$outputname = 'D:\Sec320\SYS-320\Week09\class\services.csv'
+# System Services and properties
+# Get-Service | get-member
+Get-Service | Select-Object Status, Name, DisplayName, BinaryPathName | Export-Csv -Path $outputname
+
+# Check for output file
+if ( Test-Path -Path $outputname) {
+
+    Write-Host -BackgroundColor "Red" -foregroundcolor "white" "Services File was Created"
+}
